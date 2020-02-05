79034832f8cc90cae12ef645c6e612099705f2fd
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-02-05 08:46:00 -0300

    Starting Challenge-04

--- challenge-04/challenge-04.js
+++ challenge-04/challenge-04.js
@@ -107,7 +107,9 @@ mostrar quantos assentos ainda podem ser ocupados, com a frase:
 - Se couber somente mais uma pessoa, mostrar a palavra "pessoa" no retorno
 citado acima, no lugar de "pessoas".
 */
-
+var adicionarPessoas = function(quantidadeDePessoas){
+    
+}
 
 /*
 Agora vamos verificar algumas informações do carro. Para as respostas abaixo,
