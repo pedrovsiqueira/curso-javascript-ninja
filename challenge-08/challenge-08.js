128cb00284fde8dff7402e4de3044b95807f9330
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-02-18 00:44:10 -0300

    Starting challenge-08

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,14 +3,17 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+let sum = function calculateSum(x, y){
+  return x + y
+}
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+sum(10,10)
+console.log(sum(10,10))
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
